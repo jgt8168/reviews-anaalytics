90dec2cdf5281eab372cf55bb635023f12badf3d
jgt8168 <jgt8168@hotmail.com>
2024-06-15 17:16:10 +0800

    first editon

---
+++ read.py
@@ -0,0 +1,13 @@
+data=[]
+count=0
+sum=0
+with open ('reviews.txt','r') as f:
+    for line in f:
+        data.append(line)
+        count+=1
+        sum+=len(line)
+        if count %1000==0:
+            print(count)
+avg=sum/count       
+print('檔案讀取完了，總共有',len(data),'筆資料')
+print('留言的平均長度為',avg)
