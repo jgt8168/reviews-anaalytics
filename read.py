e71a2bb4c0e147961c2b202fa8a6579f2ab1e035
jgt8168 <jgt8168@hotmail.com>
2024-06-15 17:25:01 +0800

    final edition

--- read.py
+++ read.py
@@ -11,3 +11,8 @@ with open ('reviews.txt','r') as f:
 avg=sum/count       
 print('檔案讀取完了，總共有',len(data),'筆資料')
 print('留言的平均長度為',avg)
+new=[]
+for d in data:
+    if len(d) <100:
+        new.append(d)
+print('一共有',len(new),'筆留言長度小於100')
